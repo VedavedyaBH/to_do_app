0a1ceaf69f722023e4707dd34da23d057e029167
vedavedyabh <vedavedya007@gmail.com>
2024-01-13 22:24:57 +0530

    updated Auth

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,8 +4,8 @@ const token = require('../services/jwtTokenService')
 
 exports.getUserbyId = async (req, res) => {
     try {
-        const user_id = req.params.id;
-        const userData = await userServices.getUserById({ user_id: user_id });
+        const id = req.params.id;
+        const userData = await userServices.getUserById({ id: id });
 
         if (!userData) {
             return res.status(404).send({ error: 'User not found!' })
@@ -23,9 +23,9 @@ exports.getUserbyId = async (req, res) => {
 
 exports.getUserbyName = async (req, res) => {
     try {
-        const { username } = req.headers;
-        console.log(username)
-        const userData = await userServices.getUserByName({ username: username });
+        const { user_name } = req.headers;
+        console.log(user_name)
+        const userData = await userServices.getUserIdByName({ user_name: user_name });
 
         if (!userData) {
             return res.status(404).send({ error: 'User not found!' })
@@ -44,15 +44,26 @@ exports.getUserbyName = async (req, res) => {
 exports.createNewUser = async (req, res) => {
     try {
         const { username, password } = req.headers;
+
+        console.log("__________From CreateNewUser__________")
+        console.log("********************")
         console.log({ username, password });
+        console.log("*********From CreateNewUser***********")
+        console.log("____________________")
+        
         const userData = await userServices.createNewUser({ username: username, password: password });
+        // console.log(userData)
 
         if (!userData) {
             return res.status(404).send({ error: 'User not created!' })
         }
-        return res.status(200).send({
-            message: "Done!"
-        })
+
+        if (userData) {
+            const id = await userServices.getUserIdByName({ username: username })
+            const genToken = await token.createToken({ id: id })
+            console.log(genToken)
+            return res.status(200).send({ Token: genToken, message: 'created and logged in' })
+        }
 
     } catch (error) {
         return res.status(400).send({
@@ -74,12 +85,9 @@ exports.userLogin = async (req, res) => {
 
 
         if (user_data) {
-            console.log("user_id")
-            const user = await userServices.getUserByName({username: username})
-            console.log(user.id)
-            const genToken = await token.createToken({ user: user })
-            console.log(genToken)
-            return res.status(200).send({ message: 'logged in' })
+            const id = await userServices.getUserIdByName({ username: username })
+            const genToken = await token.createToken({ id: id })
+            return res.status(200).send({ Token: genToken, message: 'logged in' })
         }
 
 
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -4,11 +4,11 @@ const jwt = require('jsonwebtoken');
 const uuid = require('uuid')
 
 
-exports.getUserById = async ({ user_id }) => {
+exports.getUserById = async ({ id }) => {
     try {
         const user_data = await db("user")
             .select("*")
-            .where("id", user_id)
+            .where("id", id)
 
         return user_data[0];
 
@@ -18,9 +18,36 @@ exports.getUserById = async ({ user_id }) => {
     }
 }
 
+exports.getUserIdByName = async ({ username: username }) => {
+    try {
+        console.log("_________From getUserIdByName___________")
+        console.log("********************")
+        console.log(username)
+        console.log("********From getUserIdByName************")
+        console.log("____________________")
+
+
+        const user_data = await db("user")
+            .select("id")
+            .where("user_name", username)
+        console.log("Hii")
+
+        if (user_data.length > 0) {
+            return user_data;
+        }
+        else {
+            return false;
+        }
+
+    }
+    catch (error) {
+        throw new Error('Failed to fetch')
+
+    }
+}
+
 exports.getUserByName = async ({ username }) => {
     try {
-
         const user_data = await db("user")
             .select("*")
             .where("user_name", username)
@@ -33,30 +60,36 @@ exports.getUserByName = async ({ username }) => {
     }
 }
 
-exports.createNewUser = async ({ username, password }) => {
+
+exports.createNewUser = async ({ username: username, password: password }) => {
     try {
-        const existingUser = await this.getUserByName({ username })
+        const existingUser = await this.getUserIdByName({ username: username })
+        console.log(existingUser)
+        console.log(username)
 
         if (existingUser) {
             throw new ReferenceError
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+        if (!existingUser) {
+            const hashedPassword = await bcrypt.hash(password, 10)
 
-        const user_data = await db("user")
-            .insert({
-                id: uuid.v4(),
-                user_name: username,
-                password: hashedPassword
-            })
-
-        return user_data;
+            const user_data = await db("user")
+                .insert({
+                    id: uuid.v4(),
+                    user_name: username,
+                    password: hashedPassword
+                })
+            return user_data;
+        }
 
     } catch (error) {
-        if (ReferenceError) {
-            throw new Error('Use diff username')
+
+        if (!ReferenceError) {
+            throw new Error('Failed to create a user')
         }
-        throw new Error('Failed to create a user')
+
+        throw new Error('Use diff username')
     }
 }
 
