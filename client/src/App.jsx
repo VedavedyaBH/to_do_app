a962a8f9f1a617e70932d30e1857d979ac36bc25
vedavedyabh <vedavedya007@gmail.com>
2024-01-16 00:33:32 +0530

    added client side, gitignore, cleaned a little

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { ToDo } from './components/ToDo'
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8086/api/to_do/", {
+      method: 'get',
+      headers: new Headers({
+        'userid': "ddfef708-56c8-4566-bba3-ceb3792b33b6"
+      }),
+    })
+      .then(async function (res) {
+        const json = await res.json();
+        setTodo(json.data);
+      })
+
+  }, []);
+
+
+  return (
+
+    <div>
+      <ToDo todo={todo}></ToDo>
+    </div >
+
+  )
+}
+
+export default App
