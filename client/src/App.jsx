5aed9a780eee949f47eadec5c257891f8e9db94e
vedavedyabh <vedavedya007@gmail.com>
2024-01-17 00:37:04 +0530

    can display to_dos, can delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { ToDo } from './components/ToDo'
+import { CreateToDo } from "./components/CreateToDo";
 import React, { useEffect, useState } from "react";
 
 function App() {
@@ -24,7 +25,8 @@ function App() {
   return (
 
     <div>
-      <ToDo todo={todo}></ToDo>
+      <CreateToDo></CreateToDo>
+      <ToDo todo={todo} ></ToDo>
     </div >
 
   )
