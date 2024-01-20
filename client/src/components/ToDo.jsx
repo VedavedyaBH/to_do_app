76f29db02efec079e435f05fc3b9a2148743005f
vedavedyabh <vedavedya007@gmail.com>
2024-01-21 00:37:48 +0530

    formatted, few changes

--- client/src/components/ToDo.jsx
+++ client/src/components/ToDo.jsx
@@ -1,44 +1,52 @@
 import React, { useEffect, useState } from "react";
-export function ToDo({ todo }) {
+export function ToDo() {
+  const [todo, setTodo] = useState([]);
 
+  useEffect(() => {
+    fetchToDo();
+  }, []);
 
-    function deleteToDo() {
-        console.log(todo[0].id)
-        fetch("http://localhost:8086/api/to_do/getTodo", {
-            method: "delete",
-            body: JSON.stringify([{
-                id: todo[0].id
-            }]),
-            headers: ({
-                'Content-Type': 'application/json',
-                'userid': "ddfef708-56c8-4566-bba3-ceb3792b33b6"
-            })
-        }).then(function () {
-            alert("deleted")
-            onDelete()
+  const fetchToDo = () => {
+    fetch("http://localhost:8086/api/to_do/", {
+      method: "get",
+      headers: new Headers({
+        userid: "ddfef708-56c8-4566-bba3-ceb3792b33b6",
+      }),
+    }).then(async function (res) {
+      const json = await res.json();
+      setTodo(json.data);
+    });
+  };
 
-        })
-    }
+  const deleteToDo = (todo) => {
+    console.log(todo.id);
+    fetch("http://localhost:8086/api/to_do/getTodo", {
+      method: "delete",
+      body: JSON.stringify([
+        {
+          id: todo.id,
+        },
+      ]),
+      headers: {
+        "Content-Type": "application/json",
+        userid: "ddfef708-56c8-4566-bba3-ceb3792b33b6",
+      },
+    }).then(async function (res) {
+      fetchToDo();
+    });
+  };
 
-
-    return <div>
-        {todo?.map(function display(to_do) {
-            return <div>
-                <h1>{to_do.title}</h1>
-                <h1>{to_do.description}</h1>
-                <button>Completed</button>
-                <button>Mark as Done</button>
-                <button>Update</button>
-                <button onClick={deleteToDo}>Delete</button>
-            </div>
-           
-        })}
-
-    </div>
-
-
-
-
-
-
+  return (
+    <>
+      {todo?.map((to_do) => (
+        <div>
+          <h1 key={to_do.title}>{to_do.title}</h1>
+          <p key={to_do.description}>{to_do.description}</p>
+          <button>Mark as Done</button>
+          <button>Update</button>
+          <button onClick={() => deleteToDo(to_do)}>Delete</button>
+        </div>
+      ))}
+    </>
+  );
 }
